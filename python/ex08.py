399aed7a163844cb9c78c75e819ea1d29a6704ba
Zach Fedor <zachfedor@gmail.com>
2020-01-09 11:32:56 -0500

    Update week 1 lectures

---
+++ python/ex08.py
@@ -0,0 +1,9 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJune\nJuly"
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+""")
